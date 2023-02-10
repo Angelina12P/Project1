6f7192c2ffd1036dbc7a517aa3174066ffb60450
Leonawesley <leonajenishlin@gmail.com>
2023-02-10 10:52:49 +0000

    added light/dark mode and used local storage for data persistent

--- assets/script.js
+++ assets/script.js
@@ -99,6 +99,33 @@ console.log(bookImage)
 
 })
 
+//For light and dark mode and storing the preference in local storage
+
+const modeBtn = document.getElementById('mode');
+modeBtn.onchange = (e) => {
+  if (modeBtn.checked === true) {
+    document.documentElement.classList.remove("light")
+    document.documentElement.classList.add("dark")
+    window.localStorage.setItem('mode', 'dark');
+  } else {
+    document.documentElement.classList.remove("dark")
+    document.documentElement.classList.add("light")
+    window.localStorage.setItem('mode', 'light');
+  }
+}
+
+const mode = window.localStorage.getItem('mode');
+if (mode == 'dark') {
+  modeBtn.checked = true;
+  document.documentElement.classList.remove("light")
+  document.documentElement.classList.add("dark")
+}
+
+if (mode == 'light') {
+  modeBtn.checked = false;
+  document.documentElement.classList.remove("dark")
+  document.documentElement.classList.add("light")
+}
 
 
 // Then use these, and pick what we want to show from these 
